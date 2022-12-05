18957e8c0064c463aa9e565d520a0090052b830a
Ivan <newzorin@gmail.com>
2022-12-05 17:17:31 +0400

    newArray

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] array1 = new string[] { "зеленый", "три", "3?", "ывнтпм", "fr" };
+    
