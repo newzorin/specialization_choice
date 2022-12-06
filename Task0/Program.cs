34ccea1e7ef5933983a877fe5d2172802a3fd7ae
Ivan <newzorin@gmail.com>
2022-12-06 18:00:13 +0400

    and

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,3 +1,15 @@
-﻿
+﻿void ArrayMin3(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
+
+string[] array1 = new string[] { "зеленый", "три", "3?", "ывнтпм", "fr" };
+ArrayMin3(array1);
 
 
